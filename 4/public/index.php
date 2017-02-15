e6543a6a1c0f0c31cc68a14eaa2e423521792a7b
Maciej Lukianski <maciej.lukianski@droptica.com>
2017-02-15 13:40:03 +0100

    add one step with docker compose and 2 different databases

--- 4/public/index.php
+++ 4/public/index.php
@@ -1,22 +1,45 @@
+<h1>this is a test of php index  with mysql !</h1>
+
+ <?php
+
+$servername = "mysql5";
+$username = "root";
+$password = "123";
+$dbname = "db";
+
+try {
+    $pdo = new PDO("mysql:host=$servername", $username, $password);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->query("CREATE DATABASE IF NOT EXISTS $dbname");
+    $pdo->query("use $dbname");
+    echo "Connected successfully to mysql 5.6";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
+
+<br/>
+
 <?php
 
-/**
- * @file
- * The PHP page that serves all page requests on a Drupal installation.
- *
- * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
- */
+$servername = "mysql8";
+$username = "root";
+$password = "123";
+$dbname = "db";
 
-use Drupal\Core\DrupalKernel;
-use Symfony\Component\HttpFoundation\Request;
-
-$autoloader = require_once 'autoload.php';
-
-$kernel = new DrupalKernel('prod', $autoloader);
-
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-
-$kernel->terminate($request, $response);
+try {
+    $pdo = new PDO("mysql:host=$servername", $username, $password);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->query("CREATE DATABASE IF NOT EXISTS $dbname");
+    $pdo->query("use $dbname");
+    echo "Connected successfully also to mysql 8";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?> 
